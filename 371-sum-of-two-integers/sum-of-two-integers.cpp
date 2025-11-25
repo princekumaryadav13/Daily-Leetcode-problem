d7077f8e5e0415f6720d5c0918a0f967eee6ebc8
princekumaryadav13 <py964830@gmail.com>
2025-11-25 10:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (64.99%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry;
+        while(b !=0){
+            carry = (a&b)<<1;
+            a= a^b;
+            b = carry;
+        }
+        return a;
+    }
+};
