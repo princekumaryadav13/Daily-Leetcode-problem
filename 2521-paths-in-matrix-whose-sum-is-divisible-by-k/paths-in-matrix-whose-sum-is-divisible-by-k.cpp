1ee73d5ff50fa6e193ea360442fa2b7400c3ea5b
princekumaryadav13 <py964830@gmail.com>
2025-11-26 19:18:08 +0530

    Time: 259 ms (61.14%) | Memory: 139.7 MB (39.34%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-     static const int MOD = 1e9 + 7;
-    int directions[2][2] = {{0, 1}, {1, 0}  } ;  //   , {0, -1}, {-1, 0}};
-    int solve(vector<vector<int>>& grid, int k, int i, int j, int path_sum ,vector<vector<vector<int>>>&dp) {
-         path_sum = (path_sum + grid[i][j]) % k;
-        if (i == grid.size() - 1 && j == grid[0].size() - 1) {
-            if (path_sum  == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-         int &ans = dp[i][j][path_sum ];
-        if (ans != -1) return ans;
-
-        
-        long long count = 0;
-        for (auto& dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
-            if (i_ >= 0 && j_ >= 0 && i_ < grid.size() && j_ < grid[0].size()) {
-                count += solve(grid, k, i_, j_, path_sum ,dp);
-            }
-        }
-        ans = count % MOD;
-        return ans ;
+    int MOD = 1e9 + 7;
+    int solve(int i,int j,long long sum,int n,int m,int k,vector<vector<int>> 
+                &grid,vector<vector<vector<int>>> &dp)
+    {
+        if(i == n-1 && j == m-1)
+            return (sum + grid[i][j])%k == 0;
+        if(dp[i][j][sum] != -1)
+            return dp[i][j][sum];
+        long long pick = 0;
+        if(i+1<n)
+            pick += solve(i+1,j,(sum+grid[i][j])%k,n,m,k,grid,dp);
+        if(j+1<m)
+            pick += solve(i,j+1,(sum+grid[i][j])%k,n,m,k,grid,dp);
+        return dp[i][j][sum] = pick % MOD;
     }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-       vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(k, -1)));
-        return solve(grid, k, 0, 0, 0,dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(k,-1)));
+        return solve(0,0,0,n,m,k,grid,dp);
     }
 };
