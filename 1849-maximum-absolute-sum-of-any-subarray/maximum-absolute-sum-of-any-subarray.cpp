596bac1bcc99669b76036dd422d371d0c17d29dc
princekumaryadav13 <py964830@gmail.com>
2025-11-27 19:13:01 +0530

    Time: 3 ms (19.68%) | Memory: 45 MB (87.13%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -9,9 +9,10 @@ public:
         int currsummax = nums[0];
         int currsummin = nums[0];
         for(int i=1;i<n;i++){
+            //kadane algo for maxsum
            currsummax = max(nums[i],currsummax + nums[i]);
            maxsum = max(maxsum,currsummax);
-
+           // kadane algo for min sum;
            currsummin = min(nums[i],currsummin + nums[i]);
            minsum = min(minsum,currsummin);
         }
