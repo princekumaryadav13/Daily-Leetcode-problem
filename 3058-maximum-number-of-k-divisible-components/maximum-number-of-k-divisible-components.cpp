8041a520ae1810e6b6ac9a73f6d943f8cc49fe44
princekumaryadav13 <py964830@gmail.com>
2025-11-28 22:00:21 +0530

    Time: 181 ms (19.93%) | Memory: 193.9 MB (13.52%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int count;
+    int dfs(int curr , int parent , unordered_map<int,vector<int>>&adj,vector<int>&values , int k){
+       int sum  = values[curr];
+
+       for(int &ngb : adj[curr]){
+        if(ngb != parent){
+            sum += dfs(ngb , curr, adj , values , k);
+            sum %= k;
+        }
+       }
+       sum %= k;
+       if(sum ==0) count++;
+
+       return sum ;
+    }
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+      unordered_map<int,vector<int>>adj;
+
+      for(auto &edge : edges){
+        int u = edge[0];
+        int v = edge[1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+      } 
+       count =0;
+       dfs(0,-1,adj,values,k);
+
+       return count;
+    }
+};
