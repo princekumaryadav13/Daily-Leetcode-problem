a869c7035c8986db8255bc72afdf4e20e90d942b
princekumaryadav13 <py964830@gmail.com>
2025-12-05 00:19:44 +0530

    Time: 9 ms (72.46%) | Memory: 19.4 MB (50.72%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int count = 0;
-        stack<char> st;
-        for (char c : directions) {
-            if (c == 'R') {
-                st.push('R');
-            } else if (c == 'S') {
-                while (!st.empty() && st.top() == 'R') {
-                    st.pop();
-                    count += 1;
-                }
-                st.push('S');
-            } else {
-                if (st.empty() || st.top() == 'L') {
-                    st.push('L');
-                } else if (st.top() == 'S') {
-                    count++;
-                    st.push('S');
+      int n = directions.size();
+      int l=0,r=n-1;
+      while(l<n && directions[l]=='L'){
+      l++;
+      }
+      while(r>=0 && directions[r]=='R'){
+      r--;
+      }
+      if(l>r) return 0;
 
-                } else {
-                    count += 2;
-                    st.pop();
-                    while (!st.empty() && st.top() == 'R') {
-                        st.pop();
-                        count++;
-                    }
-                    st.push('S');
-                }
-            }
-        }
-        return count;
+      int count=0;
+      for(int i=l;i<=r;i++){
+       if(directions[i]!='S') count++;
+       }
+       return count;
     }
 };
