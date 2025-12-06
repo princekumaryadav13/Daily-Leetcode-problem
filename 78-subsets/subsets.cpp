1d9e3d7415a9d69a83df9631bffa98cf70ed7299
princekumaryadav13 <py964830@gmail.com>
2025-12-06 21:20:06 +0530

    Time: 4 ms (9.56%) | Memory: 15.8 MB (7.20%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+     vector<vector<int>>ans;
+     void sub(vector<int>&nums,int i,vector<int>temp){
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+       
+        sub(nums,i+1,temp); //eclude the current element
+         temp.push_back(nums[i]);
+        sub(nums,i+1,temp);
+        temp.pop_back();  //include the current element
+     }
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<int>temp;
+      sub(nums,0,temp);
+      return ans;  
+    }
+};
