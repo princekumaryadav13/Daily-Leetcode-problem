8472ae3238b5c11fc9dafd61ee0ac4455859b533
princekumaryadav13 <py964830@gmail.com>
2025-12-08 20:22:52 +0530

    Time: 6 ms (54.01%) | Memory: 11.2 MB (41.61%) - LeetSync

--- 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
@@ -4,21 +4,17 @@ using namespace std;
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
-        unordered_map<int, long long> freq;
-        for (int x : nums) freq[x]++;
-
-        long long total = nums.size();
-        long long left_sum = 0;
-        long long ans = 0;
-
-        // iterate over distinct values
-        for (auto &p : freq) {
-            long long ci = p.second;
-            long long right_sum = total - left_sum - ci;
-            ans += left_sum * ci * right_sum;
-            left_sum += ci;
+        int count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if( nums[i] != nums[j] && nums[i] != nums[k]&& nums[j] != nums[k]){
+                        count++;
+                    }
+                }
+            }
         }
-
-        return (int)ans; // fits within int for given constraints, but computed in long long
+        return count;
     }
 };
