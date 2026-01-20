bdc3cceeac31e2d8666ccf85cd1dd4c09c5eaa04
princekumaryadav13 <py964830@gmail.com>
2026-01-20 12:30:16 +0530

    Time: 11 ms (10.06%) | Memory: 25.1 MB (55.03%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -4,21 +4,21 @@ public:
         vector<int> ans;
 
         for (int i = 0; i < nums.size(); i++) {
-            bool found = false;
-            for (int j = 1; j <= nums[i]; j++) {
+            //  bool found = false;
+            for (int j = 1; j <= nums[i] + 1; j++) {
                 if ((j | (j + 1)) == nums[i]) {
                     ans.push_back(j);
-                    found = true;
+                    //  found = true;
                     break;
                 }
-                // if(j==nums[i]+1){
-                //      ans.push_back(-1);
-                //  }
+                if (j == nums[i] + 1) {
+                    ans.push_back(-1);
+                }
             }
-            if (!found) {
-                ans.push_back(-1);
-            }
+            //  if (!found) {
+            //      ans.push_back(-1);
+            //  }
         }
-            return ans;
-        }
-    };
+        return ans;
+    }
+};
