38eb640abfb2161821efc3811078d9dcff97d5d4
princekumaryadav13 <py964830@gmail.com>
2026-02-16 22:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (73.18%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; i++) {
+            uint32_t bit = (n >> i) & 1;
+            res += (bit << (31 - i));
+        }
+        return res;
+    }
+};
