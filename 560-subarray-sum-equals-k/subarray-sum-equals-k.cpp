75f8101ae37910af40934c88b9ef27cfcdf763f6
princekumaryadav13 <py964830@gmail.com>
2026-03-17 22:51:37 +0530

    Time: 40 ms (66.04%) | Memory: 45.5 MB (53.94%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count =0;
+        int total=0; 
+        int prefixsum =0;
+        unordered_map<int,int>mp;
+        mp[0]=1;
         for(int i=0;i<nums.size();i++){
-            int total=0;
-            for(int j=i;j<nums.size();j++){
-                total +=nums[j];
-                if(total==k){
-                    count++;
-                    
-                }
-           // else if(total>k){
-           //     break;
-           // }
+            prefixsum += nums[i];
+            if(mp.find(prefixsum-k)!=mp.end()){
+                total += mp[prefixsum-k];
+                mp[prefixsum]++;
+            }
+            else{
+                mp[prefixsum]++;
             }
         }
-        return count;
+        return total;
     }
 };
