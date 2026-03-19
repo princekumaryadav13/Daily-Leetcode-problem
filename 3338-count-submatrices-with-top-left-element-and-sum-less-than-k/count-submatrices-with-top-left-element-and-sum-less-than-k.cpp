2717dfa8297d93408c5da91ce65c3052fcb44f4b
princekumaryadav13 <py964830@gmail.com>
2026-03-19 21:46:03 +0530

    Time: 21 ms (27.78%) | Memory: 90.3 MB (96.67%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int n = grid.size(), m = grid[0].size();
-        vector<int> cols(m);
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            int rows = 0;
-            for (int j = 0; j < m; j++) {
-                cols[j] += grid[i][j];
-                rows += cols[j];
-                if (rows <= k) {
-                    res++;
+        int m = grid.size();
+        int n= grid[0].size();
+
+        int count =0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i-1 >=0){
+                grid[i][j] += grid[i-1][j];
                 }
+                if(j-1 >=0){
+                grid[i][j] += grid[i][j-1];
+                }
+                if(i-1>=0 &&j-1>=0){
+                grid[i][j] -= grid[i-1][j-1];
+                }
+                if(grid[i][j]<=k){
+                    count++;
+
+                }
+
             }
         }
-        return res;
+        return count;
     }
 };
