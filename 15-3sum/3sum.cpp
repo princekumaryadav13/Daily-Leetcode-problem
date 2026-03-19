fbbea7798aa103cccf7cc10b6459122ad507546f
princekumaryadav13 <py964830@gmail.com>
2026-03-20 00:46:03 +0530

    Time: 47 ms (66.74%) | Memory: 29.2 MB (29.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate i
+            
+            int s = i + 1;
+            int l = n - 1;
+            while (s < l) {
+                int sum = nums[i] + nums[s] + nums[l];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[s], nums[l]});
+                    s++;
+                    l--;
 
-            int l = i + 1, r = nums.size() - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
+                    // skip duplicates
+                    while (s < l && nums[s] == nums[s - 1])
+                        s++;
+                    while (s < l && nums[l] == nums[l + 1])
+                        l--;
                 } else if (sum < 0) {
-                    l++;
+                    s++;
                 } else {
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-                    }
+                    l--;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
