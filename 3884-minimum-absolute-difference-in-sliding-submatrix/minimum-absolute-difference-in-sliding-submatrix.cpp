3685a611388ad98cd51cbd0938662b0002bc1d8a
princekumaryadav13 <py964830@gmail.com>
2026-03-20 13:12:28 +0530

    Time: 44 ms (45.93%) | Memory: 47.1 MB (15.56%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        vector<vector<int>> result(m-k+1, vector<int>(n-k+1, 0));
-        
-        for (int i = 0; i <= m-k; i++) {
-            for (int j = 0; j <= n-k; j++) {
-                
-                // Set for distinct + sorted
-                set<int> vals;
-                
-                for (int r = i; r <= i + k - 1; r++) {
-                    for (int c = j; c <= j + k - 1; c++) {
-                        vals.insert(grid[r][c]);
-                    }
+        int m= grid.size();
+        int n= grid[0].size();
+
+        vector<vector<int>>ans(m-k+1,vector<int>(n-k+1,0));
+
+        for(int i= 0;i <= m-k;i++){
+         for(int j=0;j <= n-k;j++){
+            set<int>ss;
+            for(int r=i;r <= i+k-1;r++){
+                for(int c=j;c <= j+k-1;c++){
+                    ss.insert(grid[r][c]);
                 }
-                
-                // If all elements were same, set has only 1 element
-                if (vals.size() == 1) {
-                    continue;
-                }
-                
-                int minAbsDiff = INT_MAX;
-                auto prev = vals.begin();
-                auto curr = next(prev);
-                
-                while (curr != vals.end()) {
-                    minAbsDiff = min(minAbsDiff, *curr - *prev);
-                    prev = curr;
-                    curr++;
-                }
-                
-                result[i][j] = minAbsDiff;
             }
+            if(ss.size()==1){
+              //  ans[i][j]==0;
+                continue;
+            }
+            int minabsdiff = INT_MAX;
+            auto prev = ss.begin();
+            auto curr = next(prev);
+            while(curr != ss.end()){
+                minabsdiff = min(minabsdiff ,*curr - *prev );
+                prev = curr;
+                curr++;
+            }
+            ans[i][j]=minabsdiff; 
+         }
         }
-        
-        return result;
+        return ans;
     }
 };
