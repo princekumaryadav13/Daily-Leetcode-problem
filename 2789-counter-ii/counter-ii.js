c1f451b49b711a429ed9c93b36bfe35a4e699998
princekumaryadav13 <py964830@gmail.com>
2026-04-03 23:36:51 +0530

    Time: 51 ms (51.42%) | Memory: 56.5 MB (65.44%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -4,18 +4,11 @@
  */
 var createCounter = function(init) {
     let a = init;
-     return {
-        increment : function(){
-            return ++a;
-        },
-        decrement : function(){
-            return --a;
-        },
-        reset : function(){
-            a = init;
-            return a;
-        }
-     };
+    return {
+        increment : ()=> ++a ,
+        decrement : ()=> --a,
+        reset : ()=> (a=init)
+    };
 };
 
 /**
