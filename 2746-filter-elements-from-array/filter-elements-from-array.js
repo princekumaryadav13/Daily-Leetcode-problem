8f0684d39248062c555aa972ce957a76701dda62
princekumaryadav13 <py964830@gmail.com>
2026-04-04 14:52:33 +0530

    Time: 54 ms (7.32%) | Memory: 52.9 MB (90.19%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+    let result = [];
+    for(let i=0;i < arr.length;i++){
+        if(fn(arr[i],i)){
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
