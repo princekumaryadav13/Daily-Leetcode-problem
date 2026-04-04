be064fabcb1df6973d3bb1116f12c1532b6a5801
princekumaryadav13 <py964830@gmail.com>
2026-04-05 01:17:51 +0530

    Time: 55 ms (9.40%) | Memory: 66.8 MB (5.13%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
-        string ans="";
+        
         int n = encodedText.size();
         int col = n/rows;
-        for(int i=0;i<col;i++){
-            for(int j =i ;j<n ; j+=col+1){
-                ans.push_back(encodedText[j]);
+        vector<vector<int>>mat(rows,vector<int>(col));
+        int index =0 ;
+        for(int i=0;i<rows;i++){
+            for(int j =0 ;j<col ; j++){
+                mat[i][j] =  encodedText[index++];
+            }
+        }
+        string ans="";
+        for(int k =0;k<col;k++){
+            int i =0;
+            int j = k;
+            while(i<rows && j<col){
+                ans.push_back(mat[i][j]);
+                i++;
+                j++;
             }
         }
         while(!ans.empty() && ans.back()==' '){
