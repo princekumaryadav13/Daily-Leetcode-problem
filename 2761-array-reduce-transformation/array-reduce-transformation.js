9e6a8402e279c4ee4d49afc2c6281b8b63d517de
princekumaryadav13 <py964830@gmail.com>
2026-04-04 15:01:52 +0530

    Time: 51 ms (25.04%) | Memory: 55.1 MB (17.09%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,9 +5,5 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let  a = init ;
-    for(let i=0;i<nums.length ;i++){
-        a = (fn(a , nums[i]))
-    }
-    return a;
+    return nums.reduce(fn,init);
 };
