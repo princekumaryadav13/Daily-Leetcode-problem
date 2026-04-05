cdc735e6f1afd8f6d695479d23e709e08f73803a
princekumaryadav13 <py964830@gmail.com>
2026-04-05 17:33:39 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (90.88%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+    int L = 0, R = 0, blank = 0;
+
+    for (char ch : moves) {
+        if (ch == 'L') L++;
+        else if (ch == 'R') R++;
+        else blank++;
+    }
+
+    return abs(L - R) + blank;
+}
+};
