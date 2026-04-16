dfcca581913e21e8e70784da136eb9f59a20f1bd
princekumaryadav13 <py964830@gmail.com>
2026-04-17 01:43:52 +0530

    Time: 862 ms (90.25%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select e.name , b.bonus from Employee e 
-left  join Bonus b
+LEFT OUTER JOIN Bonus b
 on e.empId = b.empId
 where  bonus < 1000 OR bonus IS NULL
