d363138bfd50d7015942e32d3fcc615219ee5ff2
princekumaryadav13 <py964830@gmail.com>
2026-04-19 01:07:25 +0530

    Time: 91 ms (6.34%) | Memory: 72.4 MB (5.19%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void check(vector<int>& candidates, int target,int j,int sum,vector<int>&cc){
-        if(sum>target) return ;
-        if(sum==target){
-            ans.push_back(cc);
-            return ;
-        }
-        for(int i=j;i<candidates.size();i++){
-            cc.push_back(candidates[i]);
-            check(candidates,target,i,sum+candidates[i],cc);
-           cc.pop_back();
-        }
-    }
+     vector<vector<int>>ans;
+
+     void check(vector<int>& candidates, int target , int sum , vector<int>temp,int i){
+       if(sum == target){
+        ans.push_back(temp);
+        return ;
+       }
+       if(sum>target || i==candidates.size()){
+        return ;
+       }
+        //not take 
+        check(candidates,target,sum,temp,i+1);
+        temp.push_back(candidates[i]);
+       // sum = sum+candidates[i];
+        check(candidates,target,sum+candidates[i],temp,i);
+        temp.pop_back();
+      //  sum = sum-candidates[i];
+     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
-        vector<int>cc;
-       
-        check(candidates,target,0,0,cc);
-      //  sort(ans.begin(),ans.end());
-      //  unique(ans.begin(),ans.end());
+        vector<int>temp;
+        check(candidates,target,0,temp,0);
         return ans;
     }
 };
