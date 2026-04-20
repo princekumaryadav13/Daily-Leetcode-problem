9d04e9b3e10b51a8b95a3ed7a1819cdc52074d3a
princekumaryadav13 <py964830@gmail.com>
2026-04-20 19:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (66.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n = t.size();
-          int k =-1;
-        for( int i=0;i<s.size();i++){
-             
-              bool found = true;;
-            for(int j=k+1;j<n;j++){
-                 if(s[i]==t[j]){
-                    k=j;
-                   found = false;
-                    break;
-                 }
-            }
-            if(found) return false;
+       int n = s.size();
+       int m = t.size();
+       int i=0,j=0;
+       while(i<n && j<m){
+        if(s[i]==t[j]){
+            i++;
         }
-        return true;
+        j++;
+       } 
+       if(i==n)return true;
+       else return false;
     }
-};  
+};
