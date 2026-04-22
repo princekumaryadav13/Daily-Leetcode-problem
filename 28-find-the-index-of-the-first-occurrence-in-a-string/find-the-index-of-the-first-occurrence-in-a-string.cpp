7d500d10d8489aa594d7384ebe634c213c0c1c28
princekumaryadav13 <py964830@gmail.com>
2026-04-23 01:01:16 +0530

    Time: 19 ms (15.14%) | Memory: 72.6 MB (5.27%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-//lecture 44
     int strStr(string haystack, string needle) {
-        int n = needle.size();
-       for(int i=0;i<haystack.size();i++){
-        if(haystack.substr(i,n)== needle){
-            return i;
+        int n= needle.size();
+        int i=0,j=0;
+
+        while(i<haystack.size()){
+            if(needle == haystack.substr(i,n)){
+                return i;
+            }
+            i++;
         }
-       }
-       return -1;
+        return -1;
     }
 };
