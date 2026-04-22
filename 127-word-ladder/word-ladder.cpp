6c685a382be6bf404a9328e9d0f1e18bb6ccd763
princekumaryadav13 <py964830@gmail.com>
2026-04-22 19:25:43 +0530

    Time: 47 ms (84.48%) | Memory: 21.2 MB (42.58%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    bool isOnediff(string &a , string &b){
-        int diff =0;
-        for(int i=0;i<a.size();i++){
-            if(a[i]!=b[i]) diff++;
-            if(diff>1)return false;
-        }
-        return diff ==1;
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-       int n = wordList.size();
-       vector<int>visited(n,0);
-
-       queue<pair<string,int>>q;
-       q.push({beginWord,1});
-
-       while(!q.empty()){
-        auto front = q.front();
-        q.pop();
-        string word = front.first;
-        int steps = front.second;
-
-        if(word == endWord) return steps;
-
-        for(int i=0;i<n;i++){
-            if(!visited[i] && isOnediff(word , wordList[i])){
-                visited[i]=1;
-                q.push({wordList[i],steps+1});
+        
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        
+        if(st.find(endWord) == st.end()) return 0;
+        
+        queue<pair<string,int>> q;
+        q.push({beginWord, 1});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            
+            string word = it.first;
+            int steps = it.second;
+            
+            if(word == endWord) return steps;
+            
+            for(int i = 0; i < word.size(); i++){
+                char original = word[i];
+                
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    word[i] = ch;
+                    
+                    if(st.find(word) != st.end()){
+                        q.push({word, steps + 1});
+                        st.erase(word); // mark visited
+                    }
+                }
+                
+                word[i] = original; // restore
             }
         }
-       } 
-       return 0;
+        
+        return 0;
     }
 };
