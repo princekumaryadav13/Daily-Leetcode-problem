d935f4d2dd46f8e5285ca08a5b81828f3a0ca742
princekumaryadav13 <py964830@gmail.com>
2026-04-23 16:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (12.44%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        vector<vector<int>>ans;
+        map<int,int>mp;
 
         for(int i=0;i<nums1.size();i++){
-           int a = nums1[i][0];
-           int b = nums1[i][1];
-           bool found = false;
-           for(int j=0;j<nums2.size();j++){
-            int c = nums2[j][0];
-            int d = nums2[j][1];
-            if(a==c){
-                ans.push_back({a,b+d});
-                found = true;
-            }
-           }
-           if(!found)ans.push_back({a,b});
+            mp[nums1[i][0]] = nums1[i][1];
         }
-        for(int i=0;i<nums2.size();i++){
-           int a = nums2[i][0];
-           int b = nums2[i][1];
-           bool found = false;
-           for(int j=0;j<nums1.size();j++){
-            int c = nums1[j][0];
-            int d = nums1[j][1];
-            if(a==c){
-                ans.push_back({a,b+d});
-                found = true;
+
+        for(int j=0;j<nums2.size();j++){
+            if(mp.count(nums2[j][0])){
+                mp[nums2[j][0]] += nums2[j][1];
             }
-           }
-           if(!found)ans.push_back({a,b});
+            else{
+                mp[nums2[j][0]] = nums2[j][1];
+            }
         }
-       sort(ans.begin(), ans.end());
-ans.erase(unique(ans.begin(), ans.end()), ans.end());
-        return ans;
+        vector<vector<int>> mergedArray;
+        for (auto it : mp) {
+            mergedArray.push_back({it.first, it.second});
+        }
+
+        return mergedArray;
     }
 };
