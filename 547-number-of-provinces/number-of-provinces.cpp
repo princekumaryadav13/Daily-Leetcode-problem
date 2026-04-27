31da73fbf7dd99d88660539ab76a1dc291e623c5
princekumaryadav13 <py964830@gmail.com>
2026-04-27 23:55:49 +0530

    Time: 1 ms (47.31%) | Memory: 23.7 MB (5.11%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int n ;
+    void bfs(unordered_map<int,vector<int>>&adj,int u,vector<bool>& visited){
+      queue<int> que;
+
+      que.push(u);
+      visited[u] = true;
+
+      while(!que.empty()){
+        int curr = que.front();
+        que.pop();
+        for(auto & v : adj[u]){
+            if(!visited[v]){
+               bfs(adj,v,visited);
+                
+            }
+        }
+      }
+
+
+
+       
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+      n = isConnected.size();  
+      
+      vector<bool>visited(n,false);
+      
+      //make adj
+      unordered_map<int,vector<int>> adj;
+      
+      for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+      }  
+        int count =0;
+         for(int i=0;i<n;i++){
+            if(!visited[i]){
+                bfs(adj,i,visited);
+                count++;
+            }
+         }
+         return count;
+    }
+};
