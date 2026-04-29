f35609557d7a3c53d6555776e50f725380f10947
princekumaryadav13 <py964830@gmail.com>
2026-04-29 20:08:14 +0530

    Time: 43 ms (47.33%) | Memory: 90.1 MB (55.56%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,32 +4,26 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<int> vec;
-        //O(m*n)
+         int sum =0;
+         vector<int>vec;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                vec.push_back(grid[i][j]);
+              vec.push_back(grid[i][j]);
             }
         }
+        sort(vec.begin(),vec.end());
 
-        int L = vec.size(); //m*n
-
-        //sort(begin(vec), end(vec)); //O(LlogL)
-        nth_element(begin(vec), begin(vec) + L/2, end(vec)); //O(L)
-        //{4, 2, 8, 6}
-        //{4, 2, 6, 8} - nth_element
-        //{2, 4, 6, 8} - complete sorting
-
-        int target = vec[L/2];
-        int result = 0;
-        for(int &num : vec) { //O(m*n)
-            if(num%x != target%x) {
-                return -1;
-            }
-
-            result += abs(target-num)/x;
+        int rem = vec[0] % x;
+        for(int num : vec) {
+            if(num % x != rem) return -1;
         }
+        int median = vec[vec.size()/2];
+        int op =0;
 
-        return result;
+        for(auto it : vec){
+            op += abs(it-median)/x;
+        }
+          return op;
+       
     }
 };
