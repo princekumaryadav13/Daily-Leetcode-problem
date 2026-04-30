5e9216592a2ed5dcb08fa044962af55b470aa7db
princekumaryadav13 <py964830@gmail.com>
2026-04-30 23:00:53 +0530

    Time: 22 ms (36.26%) | Memory: 85.6 MB (54.70%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        int n = s.size();
+        int m= spaces.size();
+        int i=0,j=0;
+        string ans="";
+        while(i< n || j < m){
+        if(j < m && i == spaces[j]){
+             ans += " ";
+             j++;   
+            }
+            ans += s[i];
+            i++;
+            
+        }
+        return ans;
+    }
+};
