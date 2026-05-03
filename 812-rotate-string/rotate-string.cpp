89fc19154ba9aae02b0c3aa2e555f1b38dca6aa0
princekumaryadav13 <py964830@gmail.com>
2026-05-03 11:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.87%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size())return false;
-        int n= s.size();
-         
-        vector<int>st;
-        for(int i=0;i<s.size();i++){
-            if(s[0]==goal[i]){
-               st.push_back(i);
-            }
-        }
-        bool found = false;
-        for(auto k : st){
-        for(int i=0;i<n;i++){
-            if(s[i]==goal[(i+k)%n]){
-                if(i==n-1){
-              found = true;
-              break;
-                }
-                
-            }
-            else{
-                break;
-            }
         
-         }
-        }
-        return found;
+       if (s.size() != goal.size()) return false;
+
+        // Step 2: Concatenate
+        string ss = s + s;
+
+        // Step 3: Check substring
+        return ss.find(goal) != string::npos;
+
+
     }
 };
