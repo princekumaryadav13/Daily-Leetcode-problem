c83d2e24b7602a3c8f9895a334392df633e04567
princekumaryadav13 <py964830@gmail.com>
2026-05-09 18:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (7.33%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       vector<vector<int>>mat=matrix;
-       int r=mat.size();
-       int c=mat[0].size();
-       for(int i=0;i<mat.size();i++){
-        for(int j=0;j<mat[0].size();j++){
-            matrix[i][j]= mat[c-j-1][i];
+       
+       int r=matrix.size();
+       int c=matrix[0].size();
+       vector<vector<int>>mat(r,vector<int>(c,0));
+
+       for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            mat[i][j] = matrix[c-j-1][i];
         }
        } 
+       matrix = mat;
     }
 };
