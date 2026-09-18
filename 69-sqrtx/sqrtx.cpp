60030fec3b7b209eae5bc0270f826052a7505b76
princekumaryadav13 <py964830@gmail.com>
2026-09-18 23:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.10%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0, r = x;
+         int ans ;
+        while(l<=r){
+            long long  mid = l +(r-l)/2;
+
+            if(mid * mid == x){
+                ans = mid ;
+                break;
+            }
+            else if (mid*mid <x){
+                ans = mid;
+                l = mid+1;
+            }else{
+                r= mid-1;
+            }
+        }
+        return ans;
+    }
+};
