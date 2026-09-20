c7edcfa3abb4c8b02dff73a96d8fa4136755c681
princekumaryadav13 <py964830@gmail.com>
2026-09-20 22:38:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.41%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-      int count=0;
-      int n = nums.size();
-      vector<int>num;
-      int i=0;
-      while(i<nums.size()){
-        if(nums[i]==val){
-            count++;
-           i++;
+        vector<int>arr;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                arr.push_back(nums[i]);
+            }
         }
-        else{
-            num.push_back(nums[i]);
-            i++;
-        }
-        
-       
-      }  
-      nums=num;
-      return nums.size();
+        nums= arr;
+        return arr.size();
     }
 };
