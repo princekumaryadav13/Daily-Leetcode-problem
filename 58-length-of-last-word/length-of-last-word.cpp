ee9b38b08da2555628153ea049585459c602481a
princekumaryadav13 <py964830@gmail.com>
2026-09-23 22:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (92.24%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int count =0;
+        int  i= n-1;
+       while(s[i]==' '){
+            i--;
+        }
+        while( i>=0 && s[i]!=' ' ){
+            count++ ;
+            i--;
+        }
+        return count;
+    }
+};
